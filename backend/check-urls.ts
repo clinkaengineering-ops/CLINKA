52611ad3156b6ae0be63931367e7f8b88c9507ad
Mohamed Talal <medoalashkam5@gmail.com>
2026-09-03 12:53:04 +0300

    chore: remove unused legacy scripts to clean up repository
    - Deleted unreferenced one-off root scripts (extract_placeholders, update_schema, etc)
    
    - Deleted frontend migration scripts (fix_amounts, patch_inv_statuses)
    
    - Deleted backend legacy playground/checker scripts (fix-images, check-pg, etc)
    
    - Ignored and deleted untracked archive files

--- backend/check-urls.ts
+++
@@ -1,6 +0,0 @@
-import prisma from './src/config/db';
-async function main() { 
-  const docs = await prisma.manualPaymentSubmission.findMany(); 
-  docs.forEach(d => console.log(d.id, d.proofUrl?.substring(0, 50))); 
-} 
-main().finally(() => prisma.$disconnect());
